807f4cf69c6ed20b2ee1709f8a7383cc78bc3da1
Alexander Mills <spot.enterprise@gmail.com>
2020-12-13 19:32:25 +0100

    13/2

--- Challenges/AoC13.cs
+++ Challenges/AoC13.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using AoCCore;
 
@@ -15,15 +17,15 @@ namespace Challenges
             var input = GetInput().Split("\n");
             var pos = int.Parse(input[0]);
             var data = input[1].Split(",").Where(x => !string.Equals(x, "x")).ToList();
-            var next = data.Select(x => GetMultiples(int.Parse(x)).First(y => y > pos)).Min();
+            var next = data.Select(x => (x, GetMultiples(int.Parse(x)).First(y => y > pos))).OrderBy(x => x.Item2).First();
 
-            return next * (next - pos)+"";
+            return int.Parse(next.x) * (next.Item2 - pos)+"";
         }
 
-        public IEnumerable<int> GetMultiples(int val)
+        public IEnumerable<long> GetMultiples(long val)
         {
             var v = val;
-            while (v < int.MaxValue - val)
+            while (v < long.MaxValue - val)
             {
                 yield return v;
                 v += val;
@@ -32,7 +34,26 @@ namespace Challenges
 
         public override string CalculateExtended()
         {
-            return base.CalculateExtended();
+            var input = GetInput().Split("\n");
+            var index = 0;
+            var data = input[1].Split(",").Select(x => (x, index++)).Where(x => !string.Equals(x.x, "x")).Select(x => (int.Parse(x.x), x.Item2)).ToList();
+            var high = data.Last();
+            var res = GetMultiples(high.Item1).First(x => Valid(x, (high.Item1, high.Item2), data));
+            
+            return res+"";
+        }
+
+        private bool Valid(long t, (int val, int off) high, List<(int val, int off)> data)
+        {
+            foreach (var v in data)
+            {
+                if (v.val == high.val)
+                    continue;
+                if (((t-(high.off-v.off)) % v.val) != 0)
+                    return false;
+            }
+
+            return true;
         }
     }
 }
